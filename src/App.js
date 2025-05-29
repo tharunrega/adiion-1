041d642acf2fc349eb9a6554e53fe709770b2ca1
Tharun Rega <133582992+tharunrega@users.noreply.github.com>
2025-05-29 14:29:15 +0530

    Update App.js
    importing the service pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -10,6 +9,11 @@ import About from './Pages/About/About';
 import Blog from './Pages/Blog/Blog';
 import Careers from './Pages/Careers/Careers';
 import Contact from './Pages/Contact/Contact';
+//IMPORTING 
+import AppDevelopment from './Pages/Services/components/AppDevelopment';
+import Marketing from './Pages/Services/components/marketing';
+import DataSolution from './Pages/Services/components/Datamining';
+import WebDevelopment from './Pages/Services/components/webdev'
 
 function App() {
   return (
@@ -23,8 +27,17 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/contact" element={<Contact />} />
+          
+          <Route path="/appdevelopment" element={<AppDevelopment />} />
+          <Route path="/webdevelopment" element={<WebDevelopment />} />
+          <Route path="/datasolutions" element={<DataSolution />} />
+          <Route path="/marketing" element={<Marketing />} />
+          
+
         </Routes>
+
       </main>
+      
       <Footer />
     </Router>
   );
